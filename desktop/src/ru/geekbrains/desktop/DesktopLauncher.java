e10daf3775678398fe261329ae8e2fbe56df5202
Kunakbaev Artem <kunakbaevav@gmail.com>
2018-09-19 22:57:13 +0500

    Выровнял окно для десктопа

--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import ru.geekbrains.StarGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.x = 300;
+		config.y = 100;
 		new LwjglApplication(new StarGame(), config);
 	}
 }
